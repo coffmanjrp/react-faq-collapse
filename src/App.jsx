b9434454b4d36ae20e8f7a1d121821b9b447299d
coffmanjrp <coffmanjrp@gmail.com>
2021-08-06 19:09:16 -0700

    Implement click to faq collapse function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import FAQ from './components/FAQ';
 import faqData from './utils/faqData';
 import './App.scss';
 
 function App() {
+  const [data, setData] = useState(faqData);
+
   return (
     <div className="App">
       <h1>Frequently Asked Questions</h1>
-      <FAQ data={faqData} />
+      <FAQ data={data} setData={setData} />
     </div>
   );
 }
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,15 +1,22 @@
 import { FaChevronDown, FaTimes } from 'react-icons/fa';
 import Icon from './Icon';
 
-const FAQ = ({ data }) => {
+const FAQ = ({ data, setData }) => {
+  const handleClick = (idx) => {
+    let updateData = [...data];
+    updateData[idx].active = !updateData[idx].active;
+
+    setData(updateData);
+  };
+
   return (
     <div className="faq-container">
       {data.length > 0 &&
-        data.map((faq) => (
+        data.map((faq, index) => (
           <div key={faq.id} className={`faq${faq.active ? ' active' : ''}`}>
             <h3 className="faq-title">{faq.title}</h3>
             <p className="faq-text">{faq.text}</p>
-            <button className="faq-toggle">
+            <button className="faq-toggle" onClick={() => handleClick(index)}>
               <Icon icon={<FaChevronDown />} className="fa-chevron-down" />
               <Icon icon={<FaTimes />} className="fa-times" />
             </button>
